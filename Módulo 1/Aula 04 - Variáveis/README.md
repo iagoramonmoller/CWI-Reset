62ca05be3f67b2980d2d9eef17a6f6c9964a4735
Iago Ramon Möller <80482914+iagoramonmoller@users.noreply.github.com>
2021-09-06 15:38:38 -0300

    Teste

--- Módulo 1/Aula 04 - Variáveis/README.md
+++
@@ -1,2 +0,0 @@
-```javascript
-<script async src="aula04.js" charset="utf-8"></script>```
--- Módulo 1/Aula 04 - Variáveis/aula04.js
+++ Módulo 1/Aula 04 - Variáveis/aula04.js
@@ -1,11 +1,11 @@
-let nomeCompleto = "Iago Ramon Möller";
-let apelido = "Bellz";
-let idade = 24;
-let dataNascimento = "24 de fevereiro de 1994";
-let localNascimento = "Taquara";
-let altura = 1.91;
-let trabalhando = true;
+var nomeCompleto = "Iago Ramon Möller";
+var apelido = "Bellz";
+var idade = 24;
+var dataNascimento = "24 de fevereiro de 1994";
+var localNascimento = "Taquara";
+var altura = 1.91;
+var trabalhando = true;
 
-let apresentacao = "Meu nome é " + nomeCompleto + " (sou conhecido[a] como " + apelido + ") e tenho " + idade + " anos. Nasci no dia " + dataNascimento + ", na cidade de " + localNascimento + ". Tenho " + altura + "m de altura e atualmente estou " + (trabalhando ? "empregado" : "desempregado") + "."
+var apresentacao = "Meu nome é " + nomeCompleto + " (sou conhecido[a] como " + apelido + ") e tenho " + idade + " anos. Nasci no dia " + dataNascimento + ", na cidade de " + localNascimento + ". Tenho " + altura + "m de altura e atualmente estou " + (trabalhando ? "empregado" : "desempregado") + "."
 
-console.log(apresentacao);
+document.write(apresentacao);
