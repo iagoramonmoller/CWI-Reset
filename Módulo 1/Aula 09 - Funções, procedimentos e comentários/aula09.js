5ac8fdf7723e9f33c85ea8943c433ae4c6f4b4b2
Iago Ramon Möller <80482914+iagoramonmoller@users.noreply.github.com>
2021-09-06 23:30:09 -0300

    Update Aula 9

--- Módulo 1/Aula 09 - Funções, procedimentos e comentários/aula09.js
+++ Módulo 1/Aula 09 - Funções, procedimentos e comentários/aula09.js
@@ -1,72 +1,112 @@
 //Aula:
 
+//Cria a função atravessar:
+
 function atravessar(){
     console.log("Olhar para a direita");
     console.log("Olhar para a esquerda");
     console.log("Atravessar");
     console.log();
   }
+
+//Executa a função atravessar:
   
   atravessar();
+
+//Cria a função print:
   
   function print(i){
     console.log(i);
     console.log();
   }
+
+//Executa a função print, dando valores diferentes para i:
   
-  print(5);
-  print(7);
+print(5);
+print(7);
+
+//Cria a função multiplicar e retorna o resultado da multiplicação:
   
   function multiplicar(a,b){
     return(a*b);
     console.log();
   }
   
+// Cria a variável que imprime o resultado da função multiplicar:
+
   var resultado = multiplicar (3,5);
   
-  print(resultado);
+//Imprime a variável resultado:
+
+print(resultado);
   
-  //Exercício:
+//Exercício:
+ //1.Crie uma função chamada imprimir que recebe um argumento e simplesmente imprime ele na tela;
   
-  //1.Crie uma função chamada imprimir que recebe um argumento e simplesmente imprime ele na tela;
+  function imprimir(i){
+      console.log(i)
+      console.log()
+  }
+
+  imprimir("Argumento");
   
-  var imprimir = "Variável Imprimir";
-  console.log(imprimir);
-  
-  //2.Lembra dos exercícios que propusemos na Aula 05 - Variáveis? Com base neles, faça o seguinte:
-  //Crie uma função para cada algoritmo proposto lá, exceto o número 6;
-  //Todas as funções devem retornar o valor final;
-  //Procure dar nomes que façam sentido às funções. Ex.: a função para o item 4 poderia se chamar mediaAritmetica ou calcularMediaAritmetica;
-  //Procure utilizar comentários para documentar cada método;
-  //Na função da média aritmética, evite colocar todos os itens como argumentos do método, pois isso limitaria sua utilização a um número fixo de elementos. Tente implementar uma lógica para receber um array e calcular a média em cima dele, tornando a função mais reaproveitável em diferentes situações.
-  
-  
-  
-  //3.Com as funções criadas, execute todo código abaixo e verifique se os resultados são os esperados (o resultado esperado de cada função está no comentário ao lado da chamada)
-  
+//2.Lembra dos exercícios que propusemos na Aula 05 - Variáveis? Com base neles, faça o seguinte:
+//Crie uma função para cada algoritmo proposto lá, exceto o número 6;
+
+//Comparação de nomes:
+
+  function mesmoNome(a,b){
+   return(a==b);
+  }
+
   cwi = "CWI"
   reset = "Reset"
-  imprimir(mesmoNome(cwi, cwi))   // true
-  imprimir(mesmoNome(cwi, reset)) // false
-  
-  imprimir("---")
-  
-  imprimir(maiorDeIdade(30)) // true
-  imprimir(maiorDeIdade(18)) // true
-  imprimir(maiorDeIdade(5))  // false
-  
-  imprimir("---")
-  
-  imprimir(valorComJuros(100))   // 110
-  imprimir(valorComJuros(984.5)) // 1082.95
-  
-  imprimir("---")
-  
-  imprimir(mediaAritmetica([1]))             // 1
-  imprimir(mediaAritmetica([1, 4, 10]))      // 5
-  imprimir(mediaAritmetica([1, 2, 3, 4, 5])) // 3
-  
-  imprimir("---")
-  
-  imprimir(margemBruta(1000000, 500000))      // 50
-  imprimir(margemBruta(528459.11, 632501.87)) // -19.68[...]
+
+imprimir(mesmoNome(cwi, cwi))   // true
+imprimir(mesmoNome(cwi, reset)) // false
+
+//Verificação de idade:
+
+  function maiorDeIdade(a){
+      return(a>=18)
+  }
+
+imprimir(maiorDeIdade(30)) // true
+imprimir(maiorDeIdade(18)) // true
+imprimir(maiorDeIdade(5))  // false
+
+//Valor com juros:
+
+function valorComJuros(valorDoBoleto,juros){
+    return(valorDoBoleto+(valorDoBoleto/100)*juros)
+}
+
+imprimir(valorComJuros(100,10))   // 110
+imprimir(valorComJuros(984.5,10)) // 1082.95
+
+//Média aritmética:
+
+function mediaAritmetica(a){
+    var media=0;
+    var arrayNumeros=a;
+    for(indice=0; indice<arrayNumeros.length;indice++){
+        var numeroSelecionado=indice;
+        media += (arrayNumeros[numeroSelecionado]/arrayNumeros.length);
+    }
+    return(media)
+        }
+
+imprimir(mediaAritmetica([1]))             // 1
+imprimir(mediaAritmetica([1, 4, 10]))      // 5
+imprimir(mediaAritmetica([1, 2, 3, 4, 5])) // 3
+
+//Cálculo margem bruta:
+
+function margemBruta(receitaLiquidaDeVendas,custoProdutosVendidos){
+    var lucroBrutoDeEmpresa = (receitaLiquidaDeVendas - custoProdutosVendidos);
+    var margemBrutaDeEmpresa = ((lucroBrutoDeEmpresa / receitaLiquidaDeVendas)*100);
+    return(margemBrutaDeEmpresa+"%")
+}
+
+imprimir(margemBruta(1000000, 500000))      // 50
+imprimir(margemBruta(528459.11, 632501.87)) // -19.68[...]
--- Módulo 1/Aula 09 - Funções, procedimentos e comentários/aula09.md
+++ Módulo 1/Aula 09 - Funções, procedimentos e comentários/aula09.md
@@ -1,8 +1,109 @@
 # Aula #09 - Funções, procedimentos e comentários
-## Teoria
 ## Exercício proposto
-## Processo
+1) Crie uma função chamada ```imprimir``` que recebe um argumento e simplesmente imprime ele na tela;
+2) Lembra dos exercícios que propusemos na Aula 05 - Variáveis? Com base neles, faça o seguinte:
+- Crie uma função para cada algoritmo proposto lá, exceto o número ```6```;
+- Todas as funções devem retornar o valor final;
+- Procure dar nomes que façam sentido às funções. Ex.: a função para o item ```4``` poderia se chamar ```mediaAritmetica``` ou ```calcularMediaAritmetica```;
+- Procure utilizar comentários para documentar cada método;
+- Na função da média aritmética, evite colocar todos os itens como argumentos do método, pois isso limitaria sua utilização a um número fixo de elementos. Tente implementar uma lógica para receber um array e calcular a média em cima dele, tornando a função mais reaproveitável em diferentes situações.
+3) Com as funções criadas, execute todo código abaixo e verifique se os resultados são os esperados (o resultado esperado de cada função está no comentário ao lado da chamada):
+```javascript
+cwi = "CWI"
+reset = "Reset"
+imprimir(mesmoNome(cwi, cwi))   // true
+imprimir(mesmoNome(cwi, reset)) // false
+
+imprimir("---")
+
+imprimir(maiorDeIdade(30)) // true
+imprimir(maiorDeIdade(18)) // true
+imprimir(maiorDeIdade(5))  // false
+
+imprimir("---")
+
+imprimir(valorComJuros(100))   // 110
+imprimir(valorComJuros(984.5)) // 1082.95
+
+imprimir("---")
+
+imprimir(mediaAritmetica([1]))             // 1
+imprimir(mediaAritmetica([1, 4, 10]))      // 5
+imprimir(mediaAritmetica([1, 2, 3, 4, 5])) // 3
+
+imprimir("---")
+
+imprimir(margemBruta(1000000, 500000))      // 50
+imprimir(margemBruta(528459.11, 632501.87)) // -19.68[...]
+```
 ## Solução
-## Descobertas
-## Dúvidas/Problemas
-## Referências
+1) Função de impressão:
+```javascript
+  function imprimir(i){
+      console.log(i)
+      console.log()
+  }
+
+  imprimir("Argumento");
+```
+2) Funções a partir do exercício de variáveis:
+
+Função 1:
+```javascript
+  function mesmoNome(a,b){
+   return(a==b);
+  }
+
+  cwi = "CWI"
+  reset = "Reset"
+
+imprimir(mesmoNome(cwi, cwi))   // true
+imprimir(mesmoNome(cwi, reset)) // false
+```
+Função 2:
+```javascript
+  function maiorDeIdade(a){
+      return(a>=18)
+  }
+
+imprimir(maiorDeIdade(30)) // true
+imprimir(maiorDeIdade(18)) // true
+imprimir(maiorDeIdade(5))  // false
+```
+Função 3:
+```javascript
+function valorComJuros(valorDoBoleto,juros){
+    return(valorDoBoleto+(valorDoBoleto/100)*juros)
+}
+
+imprimir(valorComJuros(100,10))   // 110
+imprimir(valorComJuros(984.5,10)) // 1082.95
+```
+Função 4:
+```javascript
+
+function mediaAritmetica(a){
+    var media=0;
+    var arrayNumeros=a;
+    for(indice=0; indice<arrayNumeros.length;indice++){
+        var numeroSelecionado=indice;
+        media += (arrayNumeros[numeroSelecionado]/arrayNumeros.length);
+    }
+    return(media)
+        }
+
+imprimir(mediaAritmetica([1]))             // 1
+imprimir(mediaAritmetica([1, 4, 10]))      // 5
+imprimir(mediaAritmetica([1, 2, 3, 4, 5])) // 3
+```
+Função 5:
+```javascript
+function margemBruta(receitaLiquidaDeVendas,custoProdutosVendidos){
+    var lucroBrutoDeEmpresa = (receitaLiquidaDeVendas - custoProdutosVendidos);
+    var margemBrutaDeEmpresa = ((lucroBrutoDeEmpresa / receitaLiquidaDeVendas)*100);
+    return(margemBrutaDeEmpresa+"%")
+}
+
+imprimir(margemBruta(1000000, 500000))      // 50
+imprimir(margemBruta(528459.11, 632501.87)) // -19.68[...]
+```
